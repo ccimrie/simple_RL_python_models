957890cc17f3e3be7c3e7a74f9113126a3244dee
ccimrie <imrieimrie@gmail.com>
2025-05-25 09:20:31 +0100

    Fixed formatting issue

--- DDPG/car/plot_internal_test_results.py
+++ DDPG/car/plot_internal_test_results.py
@@ -8,7 +8,7 @@ danger_time=np.mean(results[:,3])
 
 
 completed_mission_str_out=f'  - Successeful mission:  {completed_mission}'
-energy_remaining_str_out=f'   - Average energy when successful:  {energy_remaining}'
+energy_remaining_str_out=f'  - Average energy when successful:  {energy_remaining}'
 crashed_str_out=f'  - Average collision rate:  {crashed}'
 danger_time_str_out=f'  - Average time in unsafe zones:  {danger_time}'
 print(f"Results are:\n{completed_mission_str_out}\n{energy_remaining_str_out}\n{crashed_str_out}\n{danger_time_str_out}")
