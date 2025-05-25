fe05368169f4c52bae7b07f6535efe12d06fec22
Calum Imrie <calum.imrie@york.ac.uk>
2025-05-25 05:41:17 +0100

    Fixed version

--- DDPG/car/development_log_results.py
+++ DDPG/car/development_log_results.py
@@ -14,6 +14,8 @@ reward_colour=(0.2,0.2,0.8)
 success_colour=(0.2,0.8,0.2)
 fail_colour=(0.8,0.2,0.2)
 
+plt.rcParams['font.size']=20
+
 reward=np.loadtxt("rewards/1_1_1.txt")
 e_outcome=np.loadtxt("tracked_training_values/e_outcome.txt")
 u_time=np.loadtxt("tracked_training_values/u_time.txt")
@@ -23,14 +25,24 @@ ax_outcome=ax_reward.twinx()
 
 ## reward=blue, success=green, crash=red
 ee_length=np.arange(len(reward))
+lw=4.0
 
 sma_reward=smooth_average_sma(reward, 5)
-ax_reward.plot(ee_length, sma_reward, c=reward_colour, alpha=1.0, label='episode reward')
-ax_reward.plot(ee_length, reward, c=reward_colour, alpha=0.4)
+ax_reward.plot(ee_length, sma_reward, c=reward_colour, linewidth=lw, alpha=1.0, label='episode reward')
+ax_reward.plot(ee_length, reward, c=reward_colour, linewidth=lw, alpha=0.4)
+ax_reward.plot(0, 0, c=success_colour, linewidth=lw, label='cumulative goal success')
+ax_reward.plot(0, 0, c=fail_colour, linewidth=lw, label='cumulative collisions')
 
-success_count=[np.sum(e_outcome[:i+1,0]>0) for i in np.arange(ee_length)]
-fail_count=[np.sum(e_outcome[:i+1,0]<0) for i in np.arange(ee_length)]
-ax_outcome.plot(ee_length, success_count, c=success_colour, label='cumulative collisions')
-ax_outcome.plot(ee_length, fail_count, c=fail_colour, label='cumulative collisions')
+success=e_outcome[:,0]>0
+collisions=e_outcome[:,0]<0
+success_count=[np.sum(success[:i+1]) for i in ee_length]
+fail_count=[np.sum(collisions[:i+1]) for i in ee_length]
+ax_outcome.plot(ee_length, success_count, c=success_colour, linewidth=lw, label='cumulative goal success')
+ax_outcome.plot(ee_length, fail_count, c=fail_colour, linewidth=lw, label='cumulative collisions')
+ax_reward.legend(loc='lower right')
+
+ax_reward.set_xlabel("Episode")
+ax_reward.set_ylabel("Reward")
+ax_outcome.set_ylabel("Count", rotation=270, labelpad=20)
 
 plt.show()
